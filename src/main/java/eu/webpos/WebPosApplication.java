aade86eee17eaf568e99e98008a657a97e68ae4c
Colmos <colm.osullivan4@mycit.ie>
2017-02-10 14:16:14 +0000

    remove test comment

--- src/main/java/eu/webpos/WebPosApplication.java
+++ src/main/java/eu/webpos/WebPosApplication.java
@@ -2,13 +2,12 @@ package eu.webpos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class WebPosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebPosApplication.class, args);
-		
-		// TEST CODE 13
 	}
 }
